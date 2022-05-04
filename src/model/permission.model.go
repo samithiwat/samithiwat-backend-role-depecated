d805aa40c8f829f57e7bb443ac3250efd76730e0
samithiwat <samithiwat2544@gmail.com>
2022-05-05 05:48:01 +0700

    Fix wrong type of role

--- src/model/permission.model.go
+++ src/model/permission.model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Permission struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Code  string `json:"code" gorm:"index:,unique"`
-	Roles uint   `json:"roles" gorm:"many2many:role_permission"`
+	Name  string  `json:"name"`
+	Code  string  `json:"code" gorm:"index:,unique"`
+	Roles []*Role `json:"roles" gorm:"many2many:role_permission"`
 }
